79bf971c520caae72f087f6469fce51c2b4ac059
Cameron Ryan <camrya2004@gmail.com>
2025-01-28 15:19:37 -0500

    Python and java working and interacting

--- src/.python_client.py
+++ src/.python_client.py
@@ -1,14 +1,13 @@
-import py4j;
 from py4j.java_gateway import JavaGateway
 
 # Connect to the Java Gateway
 gateway = JavaGateway()
-
 # Get the SnakeGame instance
 snake_game = gateway.entry_point
 
 # Interact with the Java code
 print("Game running:", snake_game.getState())
-snake_game.executeAction(Direction.RIGHT)
-snake_game.reset()
+direction = gateway.jvm.Model.Direction
+snake_game.executeAction(direction.RIGHT)
+print("hey")
 gateway.close()
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.io.InputStreamReader;
 import py4j.GatewayServer;
 
 import Controller.ImplSnakeController;
